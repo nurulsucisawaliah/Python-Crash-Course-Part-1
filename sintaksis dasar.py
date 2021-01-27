f2b7f839c12116fa5553613575d90185991a5092
nurulsucisawaliah <71654996+nurulsucisawaliah@users.noreply.github.com>
2021-01-27 16:33:23 +0800

    Create sintaksis dasar.py
    1. sequential
    2. percabangan
    3. perulangan

---
+++ sintaksis dasar.py
@@ -0,0 +1,20 @@
+print('Hello World')
+print('By Nurul')
+print('Tanggal 27 januari 2021')
+print('-'* 21)
+
+#percabangan
+jalan = True
+
+if jalan:
+    print('lanjut')
+else:
+    print('berhenti')
+    
+# prulangan
+# menggunakan prulangan for loop
+# ingin mencetak Happy birthday untuk 5 orang
+jumlah_ucapan = 5
+
+for x in range(1, jumlah_ucapan +1):
+    print(f'Happy birthday {x}')
